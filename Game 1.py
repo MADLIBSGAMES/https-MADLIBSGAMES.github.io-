b63cd81cc7764363e8b3482ef35ff11c52846b7b
K Yogeshwara Reddy <83354232+MADLIBSGAMES@users.noreply.github.com>
2021-05-07 14:16:50 +0530

    Add files via upload

---
+++ Game 1.py
@@ -0,0 +1,27 @@
+
+Hero_Name = input("Enter a silly name:")
+Hero_Profession = input("Enter a Unrealistic profession:")
+Country_Name = input("Enter a country name:")
+Hero_Name2 =input("Enter second silly name:")
+Hero_Name2_color = input("Enter a color:")
+Hero_Name2_body = input("Enter a adjective:")
+Fight = input("Enter a adjective:")
+Hero_girlfriend = input("Enter Third silly name:")
+Hero_colleague = input("Enter fourth silly name:")
+Facial_feature = input("Enter a facial feature:")
+Hero_Name3 = input("Enter the second silly name:")
+Country_Name_city = input("Enter a city from the country you have typed:")
+Hero_name3 = input("Enter hero's evil alter ego's name:")
+Averb = input("Enter a verb for subdueding the villian:")
+Weapon = input("Enter a noun:")
+Badass_actor = input("Formerly Badass actor now selling out:")
+A_noun = input("Enter a noun:")
+
+
+print("Meet our hero" + Hero_Name )
+print("a super-intelligent" + Hero_Profession )
+print("A run in with the" + Country_Name + "military leads him to create his alter-ego" + Hero_Name2 + ",a" + Hero_Name2_color + Hero_Name2_body + "giant capable of great destruction.")
+print("He" + Fight + "battles the military with his girfriend" + Hero_girlfriend + "hh")
+
+
+
